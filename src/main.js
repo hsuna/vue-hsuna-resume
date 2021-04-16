90f68a3c09bc3ce0ec24953ce50090a451457828
hsuna <me@hsuna.com>
2021-04-16 11:53:29 +0800

    feat: update vite

--- src/main.js
+++ src/main.js
@@ -1,11 +1,39 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import { createApp } from 'vue'
-import App from './App'
+import "fullpage.js/dist/fullpage.css";
+import "swiper/dist/css/swiper.css";
 
-/**添加-vue路由器 */
-import router from './router'
+import "./assets/style/index.less";
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+import Fullpage from "fullpage.js";
+import Swiper from "swiper";
+
+function initFullpage(options) {
+  let fullpage = new Fullpage("#fullpage", {
+    menu: "#menu",
+    // lockAnchors: true,
+    anchors: ["home", "about", "skill", "exp", "demo", "contact"],
+    licenseKey: "88888888-88888888-88888888-88888888",
+    autoScrolling: true,
+    scrollHorizontally: true,
+    ...options,
+  });
+}
+
+function initSwiper() {
+  const labels = ["2013/4", "2014/8", "2016/9"];
+  let swiper = new Swiper("#swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return `<div class="${className}"><span>${labels[index]}</span></div>`;
+      },
+    },
+  });
+}
+
+(function () {
+  initFullpage({
+    afterLoad: initSwiper,
+  });
+  document.getElementById("app").style.display = "block";
+})();
